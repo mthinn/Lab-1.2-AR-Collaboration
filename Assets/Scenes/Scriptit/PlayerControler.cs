3bdf36211be2dda121905c1cb0fbc6d4f4e49c7d
mthinn <www.ultravidmaster777@gmail.com>
2020-09-15 16:36:42 -0400

    Add files via upload

---
+++ Assets/Scenes/Scriptit/PlayerControler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControler : MonoBehaviour {
+
+    public float speed;
+    public Text countText;
+    public Text scoreText;
+    public Text winText;
+
+    private Rigidbody rb;
+    private int count;
+    private int score;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        score = 0;
+        SetCountText();
+        winText.text = "";
+
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 Movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(Movement * speed);
+
+     
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score + 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Red Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            score = score - 1;
+
+
+        }
+        if (count == 16)
+        {
+            transform.position = new Vector3(20.0f, transform.position.y, 3.0f);
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 26)
+        {
+            winText.text = "You Finished with a score of:"+ count.ToString();
+        }
+    }
+
+}
+
+
+
---
+++ Assets/Scenes/Scriptit/rotater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotater : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
+        for (int i =3; i < 5; i++) {
+            System.Console.WriteLine("{0}", i);
+        }
+        Console.ReadLine();
+
+    }
+
+}
